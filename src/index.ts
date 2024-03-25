19c7575105d8452152ec7543085ec557a46c4cca
Felix <fedecraft3312@gmail.com>
2024-03-25 11:35:37 -0300

    Going to move server_config to risotto

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
 import AppServer from "./server/AppServer";
 import LocationSelection from "./server/LocationSelection";
-import serverConfigRouter from "./routes/index";
 
 export {
-    // Discovery route
-    // Add this route to every project and they will know that the environment variables have to be updated.
-    serverConfigRouter,
-    
     // App server configuration
     AppServer,
     LocationSelection,
